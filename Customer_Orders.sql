d0ea9b9b052d834a18fd215a8bea704d54f5d7b0
hanguyen6 <hanm6686@gmail.com>
2021-09-16 13:38:14 -0400

    Create Customer_Orders.sql

---
+++ Customer_Orders.sql
@@ -0,0 +1,58 @@
+# https://leetcode.com/problems/customer-order-frequency/submissions/
+# JOIN three tables customers, products and orders to get overview of customers who purchased a number of products 
+# Calculate total spending per customer 
+# Filter by total spending > 100
+
+WITH 
+    customer_purchases AS (
+        SELECT 
+            c.customer_id,
+            c.name,
+            p.product_id,
+            p.price AS product_price,
+            EXTRACT(MONTH FROM o.order_date) AS order_month,
+            EXTRACT(YEAR FROM o.order_date) AS order_year, 
+            o.quantity AS product_quantity,
+            p.price * o.quantity AS total_prices
+        FROM Customers c 
+        JOIN Product p 
+        JOIN Orders o 
+        ON c.customer_id = o.customer_id AND p.product_id = o.product_id 
+    ), 
+    customer_purchases_monthly AS (
+        SELECT 
+            order_month,
+            order_year,
+            customer_id,
+            name,
+            SUM(total_prices) AS total_prices_monthly
+        FROM customer_purchases cp
+        GROUP BY order_year, order_month, customer_id, name
+        ORDER BY customer_id, name
+    ),
+    june_purchase_100 AS (
+        SELECT 
+            *
+        FROM customer_purchases_monthly
+        WHERE (order_year = 2020 AND order_month = 6 AND total_prices_monthly >= 100) 
+    ), 
+    july_purchase_100 AS (
+        SELECT 
+            *
+        FROM customer_purchases_monthly
+        WHERE (order_year = 2020 AND order_month = 7 AND total_prices_monthly >= 100) 
+    ) 
+
+SELECT
+    j1.customer_id,
+    j1.name
+FROM june_purchase_100 j1
+JOIN july_purchase_100 j2
+ON j1.customer_id = j2.customer_id
+
+
+
+
+
+ 
+
